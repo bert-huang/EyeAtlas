476609d8d31813aaa255decc0e7cd8bc6bda3926
mark-watkin <mark_a_watkin@hotmail.com>
2015-03-20 12:52:50 +1300

    Added simple tile view implementation to work with

--- app/src/main/java/nz/ac/aucklanduni/eyeatlas/ImageViewerActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/eyeatlas/ImageViewerActivity.java
@@ -1,39 +1,100 @@
 package nz.ac.aucklanduni.eyeatlas;
 
 import android.app.Activity;
+import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.qozix.tileview.TileView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class ImageViewerActivity extends Activity {
+
+    private TileView tileView;
+    private static final int IMAGE_SCALE = 0;
+
+    //bundle variables passed to activity
+    private int imageId;
+    private String imageName;
+    private int imageSizeX;
+    private int imageSizeY;
+    private float[] detailLevels;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_viewer);
+
+        this.unpackBundle(savedInstanceState);
+
+        tileView = new TileView( this );
+        setContentView( tileView );
+
+        tileView.setBackgroundColor(Color.BLACK);
+
+        // size of original image at 100% scale
+        tileView.setSize(imageSizeX, imageSizeY );
+
+        // detail levels
+        tileView.addDetailLevel( 0.500f, "tiles/fantasy/500/%col%_%row%.jpg", "samples/middle-earth.jpg");
+        tileView.addDetailLevel( 0.250f, "tiles/fantasy/250/%col%_%row%.jpg", "samples/middle-earth.jpg");
+        tileView.addDetailLevel( 0.125f, "tiles/fantasy/125/%col%_%row%.jpg", "samples/middle-earth.jpg");
+
+        // allow scaling past original size
+        tileView.setScaleLimits( 0, 2 );
+
+        // frame the troll
+        frameTo( imageSizeX / 2, imageSizeY / 2 );
+
+        // scale down a little
+        tileView.setScale( IMAGE_SCALE );
+
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_image_viewer, menu);
-        return true;
+    public void onPause() {
+        super.onPause();
+        tileView.clear();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onResume() {
+        super.onResume();
+        tileView.resume();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        tileView.destroy();
+        tileView = null;
+    }
 
-        return super.onOptionsItemSelected(item);
+    public TileView getTileView(){
+        return tileView;
+    }
+
+    private void unpackBundle(Bundle bundle) {
+        imageSizeY = 4057;
+        imageSizeX = 4015;
+    }
+
+    /**
+     * This is a convenience method to moveToAndCenter after layout (which won't happen if called directly in onCreate
+     * see https://github.com/moagrius/TileView/wiki/FAQ
+     */
+    private void frameTo( final double x, final double y ) {
+        getTileView().post( new Runnable() {
+            @Override
+            public void run() {
+                getTileView().moveToAndCenter( x, y );
+            }
+        });
     }
 }
