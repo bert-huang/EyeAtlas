7b969c37ae1eb31160d7f5eeb63b11b38aca919e
mark-watkin <mark_a_watkin@hotmail.com>
2015-03-30 12:22:42 +1300

    Refactored to use assert that. Removed commented code. Cleaned build file. @bert-huang.

--- app/src/test/java/nz/ac/aucklanduni/eyeatlas/util/TestFileReader.java
+++ app/src/test/java/nz/ac/aucklanduni/eyeatlas/util/TestFileReader.java
@@ -13,15 +13,19 @@ import org.robolectric.shadows.ShadowApplication;
 
 import java.io.IOException;
 
-import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 @Config(emulateSdk=18)
 @RunWith(MyRobolectricTestRunner.class)
 public class TestFileReader {
 
-    private ShadowApplication mockApplication;
     private Context mockContext;
+
+    private ShadowApplication mockApplication;
+    private final static String FILE_PATH = "test/testreader.txt";
+    private final static String EXPECTED_RESULT = "this is a test";
 
     @BeforeClass
     public static void oneTimeSetup() {}
@@ -29,32 +33,15 @@ public class TestFileReader {
     @Before
     public void setUp() {
         mockApplication = Robolectric.getShadowApplication();
-        Assert.assertNotNull(mockApplication);
-        mockContext = mockApplication.getApplicationContext();
+        Assert.assertNotNull(this.mockApplication);
+        mockContext = this.mockApplication.getApplicationContext();
         Assert.assertNotNull(mockContext);
     }
 
     @Test
     public void testFileReader() throws IOException {
-        String result = FileReader.getFileContent("test/testreader.txt", mockContext);
-        assertEquals("this is a test", result);
+        String result = FileReader.getFileContent(FILE_PATH, mockContext);
+        assertThat(result, is(EXPECTED_RESULT));
     }
 
 }
-
-//    private final static String FILE_PATH = "/Users/bert/Documents/projects/EyeAtlas/app/src/main/assets/test/testreader.txt";
-//    private final static String FILE_PATH = "test/testreader.txt";
-//
-//    @Test
-//    public void testFileReader() throws IOException {
-//        Mockito.when(FileReader.fileStreamProvider(Mockito.anyString(), Mockito.any(MockContext.class))).thenAnswer(new Answer<FileInputStream>() {
-//            @Override
-//            public FileInputStream answer(InvocationOnMock invocation) throws Throwable {
-//                Object[] args = invocation.getArguments();
-//                return new FileInputStream((String) args[0]);
-//            }
-//        });
-//
-//        String result = FileReader.getFileContent(FILE_PATH, new MockContext());
-//        assertThat(result, is("this is a test"));
-//    }
