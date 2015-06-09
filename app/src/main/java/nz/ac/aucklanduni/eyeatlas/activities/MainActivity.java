a279f6f6165e386d30333dd9056ab180a57394eb
Bert Huang <bert.huang@outlook.com>
2015-06-09 17:23:39 +1200

    Implemented navigation drawer. @mark-watkin.

--- app/src/main/java/nz/ac/aucklanduni/eyeatlas/activities/MainActivity.java
+++ app/src/main/java/nz/ac/aucklanduni/eyeatlas/activities/MainActivity.java
@@ -1,41 +1,74 @@
 package nz.ac.aucklanduni.eyeatlas.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.ListView;
 import nz.ac.aucklanduni.eyeatlas.R;
+import nz.ac.aucklanduni.eyeatlas.adapter.NavigationDrawerListAdapter;
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends ActionBarActivity {
+    private Toolbar toolbar;
+    DrawerLayout drawer;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationDrawerListAdapter adapter = new NavigationDrawerListAdapter(this, R.id.NavigationDrawerList);
+        ListView listView = (ListView) findViewById(R.id.NavigationDrawerList);
+        listView.setAdapter(adapter);
+        listView.setClickable(true);
+
+        initialiseDrawer();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void initialiseDrawer() {
+
+        drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawer,toolbar,R.string.openDrawer,R.string.closeDrawer){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+        };
+        drawer.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
 }
---
+++ app/src/main/java/nz/ac/aucklanduni/eyeatlas/adapter/NavigationDrawerListAdapter.java
@@ -0,0 +1,43 @@
+package nz.ac.aucklanduni.eyeatlas.adapter;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import nz.ac.aucklanduni.eyeatlas.R;
+
+public class NavigationDrawerListAdapter extends ArrayAdapter {
+
+    private static final int COUNT = 2;
+    private static final int ICONS[] = {R.drawable.ic_photo, R.drawable.ic_list};
+    private static final int NAMES[] = {R.string.gallery, R.string.index};
+
+
+    public NavigationDrawerListAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflator = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflator.inflate(R.layout.item_row, parent, false);
+
+        ImageView imgView = (ImageView) rowView.findViewById(R.id.rowIcon);
+        TextView txtView = (TextView) rowView.findViewById(R.id.rowText);
+
+        imgView.setImageResource(ICONS[position]);
+        txtView.setText(NAMES[position]);
+
+        return rowView;
+    }
+
+    @Override
+    public int getCount() {
+        return NavigationDrawerListAdapter.COUNT;
+    }
+}
