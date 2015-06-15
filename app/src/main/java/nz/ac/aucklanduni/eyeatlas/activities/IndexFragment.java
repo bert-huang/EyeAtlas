b629fa89d7a5dfa355c31bd747f2a7f17a0110c8
mark-watkin <mark_a_watkin@hotmail.com>
2015-06-15 15:27:59 +1200

    Implemented basic index navigation.

--- app/src/main/java/nz/ac/aucklanduni/eyeatlas/activities/IndexFragment.java
+++ app/src/main/java/nz/ac/aucklanduni/eyeatlas/activities/IndexFragment.java
@@ -1,15 +1,37 @@
 package nz.ac.aucklanduni.eyeatlas.activities;
 
 import android.app.ListFragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.List;
+
 import nz.ac.aucklanduni.eyeatlas.R;
 import nz.ac.aucklanduni.eyeatlas.adapter.CategoryAdapter;
+import nz.ac.aucklanduni.eyeatlas.model.Category;
+import nz.ac.aucklanduni.eyeatlas.model.Properties;
 
 public class IndexFragment extends ListFragment {
-    CategoryAdapter adapter;
+    private CategoryAdapter adapter;
+    private List<Category> categoryList;
+    public final static String CATEGORY_KEY = "Category";
+
+    @Override
+    public void setArguments(Bundle bundle) {
+        if (bundle != null && bundle.containsKey(CATEGORY_KEY)) {
+            this.categoryList = (List<Category>) bundle.getSerializable(CATEGORY_KEY);
+        }
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -18,12 +40,59 @@ public class IndexFragment extends ListFragment {
         adapter = new CategoryAdapter<String>(getActivity(), this.getId());
 
         setListAdapter(adapter);
+        this.setCategories();
+    }
+
+    private void setCategories() {
+        if (categoryList != null) {
+            adapter.setCategories(categoryList);
+            return;
+        }
+        new AsyncTask<Object, Object, List<Category>>() {
+            @Override
+            protected List<Category> doInBackground(Object... params) {
+                try {
+                    URL url = new URL(Properties.getInstance(IndexFragment.this.getActivity()).getHerokuUrl() + "rest/category");
+                    URLConnection conn = url.openConnection();
+                    InputStream in = conn.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String val = br.readLine().toString();
+
+                    ObjectMapper ob = new ObjectMapper ();
+                    return Arrays.asList(ob.readValue(val, Category[].class));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //Display popup and shut down the app
+                    throw new RuntimeException("Condition count could not be obtained");
+                }
+            }
+
+            @Override
+            protected void onPostExecute(List<Category> result) {
+                categoryList = result;
+                adapter.setCategories(categoryList);
+            }
+        }.execute();
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        IndexFragment nextFrag= new IndexFragment();
-        this.getFragmentManager().beginTransaction().replace(R.id.fragment_container, nextFrag, "").addToBackStack(null).commit();
+        List<Category> children = categoryList.get(position).getChildren();
+
+        if (children.isEmpty()) {
+            GalleryFragment galleryFragment = new GalleryFragment();
+            Bundle bundle = new Bundle();
+            bundle.putSerializable(CATEGORY_KEY, categoryList.get(position));
+            galleryFragment.setArguments(bundle);
+            this.getFragmentManager().beginTransaction().replace(R.id.fragment_container, galleryFragment, "").addToBackStack(null).commit();
+            return;
+        }
+
+        IndexFragment indexFragment = new IndexFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(CATEGORY_KEY, (java.io.Serializable) children);
+        indexFragment.setArguments(bundle);
+        this.getFragmentManager().beginTransaction().replace(R.id.fragment_container, indexFragment, "").addToBackStack(null).commit();
     }
 }
--- app/src/main/java/nz/ac/aucklanduni/eyeatlas/adapter/CategoryAdapter.java
+++ app/src/main/java/nz/ac/aucklanduni/eyeatlas/adapter/CategoryAdapter.java
@@ -5,10 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
 
 import nz.ac.aucklanduni.eyeatlas.R;
+import nz.ac.aucklanduni.eyeatlas.model.Category;
 
 public class CategoryAdapter<T> extends ArrayAdapter {
+    List<Category> categories;
+
     public CategoryAdapter(Context context, int resource) {
         super(context, resource);
     }
@@ -18,11 +24,25 @@ public class CategoryAdapter<T> extends ArrayAdapter {
         LayoutInflater inflator = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflator.inflate(R.layout.category_item, parent, false);
 
+        if (categories != null) {
+            TextView textView = (TextView) rowView.findViewById(R.id.cardText);
+            textView.setText(categories.get(position).getName());
+        }
+
         return rowView;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        if(categories == null) {
+            return 0;
+        } else {
+            return categories.size();
+        }
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+        this.notifyDataSetInvalidated();
     }
 }
