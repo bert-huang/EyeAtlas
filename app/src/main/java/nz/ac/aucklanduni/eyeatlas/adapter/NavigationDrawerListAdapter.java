6f31332e923a8a91303e5335ba5f0d570f879af1
Bert Huang <bert.huang@outlook.com>
2015-06-20 10:55:47 +1200

    Changed GalleryFragment from ListFragment into normal Fragment and use GridView as container instead. Change colour scheme to dark theme.

--- app/src/main/java/nz/ac/aucklanduni/eyeatlas/adapter/NavigationDrawerListAdapter.java
+++ app/src/main/java/nz/ac/aucklanduni/eyeatlas/adapter/NavigationDrawerListAdapter.java
@@ -1,20 +1,16 @@
 package nz.ac.aucklanduni.eyeatlas.adapter;
 
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import nz.ac.aucklanduni.eyeatlas.R;
 import nz.ac.aucklanduni.eyeatlas.activities.GalleryFragment;
-import nz.ac.aucklanduni.eyeatlas.activities.IndexFragment;
 
 public class NavigationDrawerListAdapter extends ArrayAdapter {
 
